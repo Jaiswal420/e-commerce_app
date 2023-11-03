9bb77e956916227f53a413b2a9a9097336383d2a
Adarsh <adarshjaiswal374@gmail.com>
2023-11-03 20:46:01 +0530

    initial commit

---
+++ src/main/java/com/sheryians/major/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.sheryians.major.repository;
+
+import com.sheryians.major.model.Product;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.metamodel.SingularAttribute;
+import java.io.Serializable;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository <Product, Long> {
+    List<Product> findAllByCategory_Id(int id);
+
+//    List<Product> findAllByCategory_Id(int id);
+}
